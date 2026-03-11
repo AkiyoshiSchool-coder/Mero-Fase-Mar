3f28cb0741d9f8a9a22fecade1441a3bb96dbb77
Pedro <pedrogurskif@gmail.com>
2026-03-11 10:34:09 -0300

    mero que anda

--- MeroFaseMar/Assets/Gurski/MeroMovement.cs
+++ MeroFaseMar/Assets/Gurski/MeroMovement.cs
@@ -1,13 +1,18 @@
+using UnityEditor.ShaderGraph.Internal;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MeroMovement : MonoBehaviour
 {
-    float speed = 20f;
+    float speed = 5f;
     public InputActionAsset InputActions;
     private InputAction moveAction;
     Vector2 direction;
     Vector2 mouseworld;
+    private float meroRotation;
+    private bool startMoving = false;
     void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
@@ -18,11 +23,20 @@ public class MeroMovement : MonoBehaviour
     {
         if(moveAction.WasPressedThisFrame())
         {
-            mouseworld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            Debug.Log(mouseworld);
-            direction = mouseworld - new Vector2(transform.position.x, transform.position.y);
+            startMoving = true;
         }
-        transform.LookAt(direction);
+        if(startMoving)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        mouseworld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        direction = mouseworld - new Vector2(transform.position.x, transform.position.y);
+        meroRotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, meroRotation - 90);
         transform.position = Vector2.MoveTowards(transform.position, mouseworld, speed*Time.deltaTime);
     }
 }
