9cf0e94d401c7673682afd75cd090a77e9028af6
Yoshi Poggers <20241ctb0180021@estudantes.ifpr.edu.br>
2026-03-18 09:55:29 -0300

    FoodspawnerSemiReady

--- MeroFaseMar/Assets/Acreano/ScriptsA/FoodSpawner.cs
+++ MeroFaseMar/Assets/Acreano/ScriptsA/FoodSpawner.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class FoodSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject FoodA;
+    public GameObject FoodB;
+    public GameObject Mero;
+
+    private Vector3 RandomPos;
+
+    private int random;
+
     void Start()
     {
         
@@ -13,4 +20,26 @@ public class FoodSpawner : MonoBehaviour
     {
         
     }
+//mathf.abs()
+    void RandomDefine(float posX, float posY)
+    {
+        posX = UnityEngine.Random.Range(9.02f,18.04f)
+    }
+    //9.02 5.13
+    void FoodPositionDecider()
+    {
+        
+    }
+    void FoodDecider()
+    {
+        random = UnityEngine.Random.Range(0,1);
+    }
+    void FoodSpawn(int rand, Vector3 randPos)
+    {
+        if(rand == 1)
+        {
+            Instantiate(FoodA, randPos, Quaternion.identity);
+        }
+    }
+
 }
