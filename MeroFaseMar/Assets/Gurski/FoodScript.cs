8e8e8f8e31229dd5148a4df6c2a01394ca0f0fb2
Pedro <pedrogurskif@gmail.com>
2026-03-18 09:15:33 -0300

    barraCode

--- MeroFaseMar/Assets/Gurski/FoodScript.cs
+++ MeroFaseMar/Assets/Gurski/FoodScript.cs
@@ -5,16 +5,37 @@ using UnityEngine.InputSystem;
 
 public class FoodScript : MonoBehaviour
 {
+    [SerializeField] int foodA = 0;
+    [SerializeField] int foodB = 0;
+    [SerializeField] int totalFood = 0;
+    public GameObject barra;
+    private BarraNivel barraCode;
+
+    void Awake()
+    {
+        barraCode = barra.GetComponent<BarraNivel>();
+    }
+
     void Update()
     {
         Debug.Log(gameObject.transform.position.z);
     }
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("aaaa");
         if(collider.gameObject.CompareTag("Head"))
         {
+            if(gameObject.CompareTag("FoodA"))
+            {
+                Debug.Log("comida A");
+                foodA++;
+            }
+            else if(gameObject.CompareTag("FoodB"))
+            {
+                Debug.Log("comida B");
+                foodB++;
+            }
             Destroy(gameObject);
+
         }
     }
 }
