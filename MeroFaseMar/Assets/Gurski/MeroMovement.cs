f6d210fcbae2442cbaf91f67a406f224003e5f57
Pedro <pedrogurskif@gmail.com>
2026-03-25 09:09:21 -0300

    marselo

--- MeroFaseMar/Assets/Gurski/MeroMovement.cs
+++ MeroFaseMar/Assets/Gurski/MeroMovement.cs
@@ -38,5 +38,21 @@ public class MeroMovement : MonoBehaviour
         meroRotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, meroRotation - 90);
         transform.position = Vector2.MoveTowards(transform.position, mouseworld, speed*Time.deltaTime);
+        if(transform.position.x > 34.3f)
+        {
+            transform.position = new Vector3(34.3f, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < -34.3f)
+        {
+            transform.position = new Vector3(-34.3f, transform.position.y, transform.position.z);
+        }
+        if(transform.position.y > 18.8f)
+        {
+            transform.position = new Vector3(transform.position.x, 18.8f, transform.position.z);
+        }
+        else if(transform.position.y < -18.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -18.8f, transform.position.z);
+        }
     }
 }
