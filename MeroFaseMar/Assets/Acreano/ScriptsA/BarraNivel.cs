9d89842b89b0f390f8aafd003bf204a97fa89a65
Yoshi Poggers <20241ctb0180021@estudantes.ifpr.edu.br>
2026-04-08 20:06:09 -0300

    corrected bars

--- MeroFaseMar/Assets/Acreano/ScriptsA/BarraNivel.cs
+++ MeroFaseMar/Assets/Acreano/ScriptsA/BarraNivel.cs
@@ -5,8 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class BarraNivel : MonoBehaviour
 {
-    private int teste = 0;
-    public int qntd;
+    private float teste = 0;
+    public float qntd;
     Scene scene;
     public GameObject gameMgr;
     private GameManager gameManager;
