aaa468bbb39cc38c1494f9c3be10e232bb1e9de0
Pedro <pedrogurskif@gmail.com>
2026-04-23 10:21:59 -0300

    cor

--- MeroFaseMar/Assets/Gurski/AcessibilityFood.cs
+++ MeroFaseMar/Assets/Gurski/AcessibilityFood.cs
@@ -1,9 +1,30 @@
+using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AcessibilityFood : MonoBehaviour
 {
-    void Awake()
+    private Color[] colorList = {Color.yellow, Color.orange, Color.red, Color.green,
+        Color.cyan, Color.blue, Color.hotPink, Color.brown};
+    public int index;
+    private int limit;
+
+    public Image sprite;
+
+    void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        sprite.color = colorList[index];
+        limit = colorList.Length;
+    }
+
+    public void changeColor()
+    {
+        index++;
+        if(index == limit)
+        {
+            index = 0;
+        }
+        sprite.color = colorList[index];
     }
 }
