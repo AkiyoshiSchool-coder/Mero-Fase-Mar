27edb9ea7d27eee0586be6e4e228fa0439ecdffa
Pedro <pedrogurskif@gmail.com>
2026-05-14 11:11:10 -0300

    barco-ainda-nao-funciona

--- MeroFaseMar/Assets/Gurski/BarcoMovement.cs
+++ MeroFaseMar/Assets/Gurski/BarcoMovement.cs
@@ -4,36 +4,25 @@ using UnityEngine.UIElements;
 
 public class BarcoMovement : MonoBehaviour
 {
-    [SerializeField] private Transform boatPosition;
-    [SerializeField] private Transform leavePosition;
     [SerializeField] private float speed;
     [SerializeField] private DiverSpawner diverSpawner;
-    [SerializeField] private Transform target;
-    private Vector2 direction;
-    private float barcoRotation;
+    [SerializeField] private Transform direction;
+    [SerializeField] private float boatDuration = 50;
+    private float timer = 0;
     void Start()
     {
-        
+        Destroy(gameObject, boatDuration);
+        transform.LookAt(direction);
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed*Time.deltaTime);
-        RotateToTarget();
-        if(transform.position == target.transform.position)
-            {
-                diverSpawner.Diver();
-                target = leavePosition;
-                direction = target.transform.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
-                barcoRotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            }
-    }
-
-    void RotateToTarget()
-    {
-        if(transform.rotation.z != barcoRotation+90)
+        Debug.Log(timer);
+        timer += Time.fixedDeltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, direction.transform.position, speed*Time.deltaTime);
+        if(timer == 20f)
         {
-            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z+1*Time.deltaTime);
+            diverSpawner.Diver();
         }
     }
 }
