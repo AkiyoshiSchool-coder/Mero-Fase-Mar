482cf6af3af855a03f66296fd65a8fa4ee6a41c8
unknown <20241ctb0180021@estudantes.ifpr.edu.br>
2026-08-20 10:29:26 -0300

    LixoBoiandoCerto

--- MeroFaseMar/Assets/Acreano/ScriptsA/TrashSpawnerMovement.cs
+++ MeroFaseMar/Assets/Acreano/ScriptsA/TrashSpawnerMovement.cs
@@ -9,36 +9,38 @@ public class TrashSpawnerMovement : MonoBehaviour
     [SerializeField] private int Decider;
     [SerializeField] private float Speed;
     [SerializeField] private bool CheckMove;
+    [SerializeField] private bool isMoving;
     void Update()
     {
         if(CheckMove)
         {
-            Movemment();
+            StartCoroutine(RandomDecider(Decider));
         }
+        Movemment();
     }
     void Movemment()
     {
-        CheckMove = false;
-        StartCoroutine(RandomDecider(Decider));
         switch (Decider)
         {
             case 1:
-
+            Debug.Log("A");
                 transform.position = Vector3.MoveTowards(transform.position, P1.transform.position ,Speed*Time.deltaTime);
                 break;
             case 2:
+            Debug.Log("B");
                 transform.position = Vector3.MoveTowards(transform.position, P2.transform.position ,Speed*Time.deltaTime);
                 break;
             case 3:
+            Debug.Log("C");
                 transform.position = Vector3.MoveTowards(transform.position, P3.transform.position ,Speed*Time.deltaTime);
                 break;
-            
         }
-        CheckMove = true;
     }
     IEnumerator RandomDecider(int decider)
     {
-        yield return new WaitForSeconds(6f);
+        CheckMove = false; 
         Decider = Random.Range(1,4);
+        yield return new WaitForSeconds(6f);
+        CheckMove = true;
     }
 }
