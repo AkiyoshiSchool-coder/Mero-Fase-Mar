8bf09d530306af9c5f3166f56281979afd9f615f
Pedro <pedrogurskif@gmail.com>
2026-09-17 07:25:11 -0300

    ajustes em script

--- MeroFaseMar/Assets/Gurski/Code/RepositionPlatform.cs
+++ MeroFaseMar/Assets/Gurski/Code/RepositionPlatform.cs
@@ -9,7 +9,7 @@ public class RepositionPlatform : MonoBehaviour
 
     void Start()
     {
-        if(Application.platform == RuntimePlatform.Android)
+        if(Application.platform.Equals(RuntimePlatform.Android))
         {
             objectPos.anchoredPosition = objectPos.anchoredPosition+pos;
             objectPos.localScale = objectPos.localScale*mobileScale;
